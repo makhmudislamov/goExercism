c61cf681303c6083052e4f5f1be2ceeed475c253
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-09-25 23:27:16 -0700

    testing

---
+++ space-age/space_age.go
@@ -0,0 +1,19 @@
+package space
+
+// formula = Age in seconds / (planet orbital period in Earth years * 31557600 (earth year in seconds))
+
+// import "math"
+
+
+// Age calculates age in seconds on different planets
+func Age(seconds float64, planet string) float64 {
+	if planet == "Earth" {
+
+		return seconds / 31557600
+	} else {
+
+		return 123
+	}
+
+	
+}
